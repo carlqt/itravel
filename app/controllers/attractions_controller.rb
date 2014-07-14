77621e1b09add2a71d8ba6571aac6e0dc2f54d2d
carl <carl@serviceseeking.com.au>
2014-07-14 10:51:38 +0800

    add attraction form

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -2,17 +2,26 @@ class AttractionsController < ApplicationController
 
   def new
     @countries = Country.all
-    @country = Country.new
-    @attraction = @country.attractions.build
+    # @attraction = @country.attractions.build
   end
 
   def create
-
+    build_data(params)
+    if @country.save
+      redirect_to attractions_new_path 
+    else
+      render :new
+    end
   end
 
   private
 
+  def build_data(params)
+    @country = Country.find params[:country]
+    @country.attractions.build(attraction_params)
+  end
+
   def attraction_params
-    params.require(:attraction).permit(:name, :description)
+    params.require(:attractions).permit(:name, :description)
   end
 end
